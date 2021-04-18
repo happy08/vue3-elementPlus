5df4f9982d21bddadf6f5748ff1ddbd6927be3d6
happy08 <happy@send2me.cc>
2021-04-18 11:23:44 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
         pathRewrite: {
           "^/api": ""
         },
-        secure: false, // 如果是https接口，需要配置这个参数为true
+        secure: false, // 1如果是https接口，需要配置这个参数为true
         changeOrigin: true // 如果接口跨域，需要进行这个参数配置为true
       },
       "/test": {
